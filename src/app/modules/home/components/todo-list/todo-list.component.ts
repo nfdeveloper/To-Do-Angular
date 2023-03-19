a6590c2560b947815b12c6508c59207b0632f180
nfdeveloper04 <nfdeveloper04@gmail.com>
2023-03-19 12:17:54 -0300

    Aplicandológicapara deleção e adição denovas Tasks

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit{
+  
+  public taskList: Array<TaskList> = [];
+  constructor() { }
+  
+  ngOnInit(): void {    
+  }
 
+  public setEmmitTaskList(event:string){
+    this.taskList.push({ task:event,checked:false })
+  }
+
+  public deleteItemTaskList(event:number){
+    this.taskList.splice(event,1);
+  }
+
+  public deleteAllTaskList(){
+    const confirm = window.confirm("Você deseja realmente deletar tudo?");
+
+    if(confirm){
+      this.taskList = [];
+    }
+  }
 }
